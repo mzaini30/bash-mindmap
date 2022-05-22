3c481d1785a896f70b4ee6dbac75f49df46cbab0
mzaini30 <muhzaini30@gmail.com>
2022-05-22 13:23:20 +0800

    Kirim

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view />
 </template>
+
+<style>
+	* {
+		word-wrap: break-word;
+	}
+	label {
+		@apply block
+	}
+	input {
+		@apply focus:outline-none block
+	}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-	<div class="prose p-3">
-		<h1>Assalamualaikum</h1>
-		<p>Welcome to this Vue template, cooked by Zen.</p>
+	<button class="fixed bottom-2 right-2 px-3 py-1 bg-orange-500 text-white rounded" to="/tambah">Add</button>
+
+	<div class="fixed top-0 left-0 w-full p-2">
+		<form class="border rounded">
+			<label>New Mindmap</label>
+			<input class="border w-full"/>
+			<input type="submit" class="px-3 py-1 bg-green-500 rounded" value="Add"/>
+		</form>
 	</div>
 </template>
